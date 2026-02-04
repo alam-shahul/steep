ee6a58ed26cdc849c3112901097cce3fa444a85f
Shahul Alam <alam.shahul@gmail.com>
2026-02-04 10:36:56 -0500

    Add STAGATEVAE test.

--- tests/test_models_graph.py
+++ tests/test_models_graph.py
@@ -1,6 +1,6 @@
 import pytest
 
-from steep.models import STAGATE
+from steep.models import STAGATE, STAGATEVAE
 
 
 @pytest.fixture
@@ -18,8 +18,32 @@ def stagate(mock_dim):
     return model
 
 
+@pytest.fixture
+def stagate_vae(mock_dim):
+    in_dim = mock_dim
+    num_hidden = mock_dim
+    out_dim = 2
+
+    model = STAGATEVAE(
+        in_dim=in_dim,
+        num_hidden=num_hidden,
+        out_dim=out_dim,
+    )
+
+    return model
+
+
 def test_stagate(stagate, mock_dim, mock_data):
     num_nodes, in_dim = mock_data.x.size()
-    node_embeddings, reconstruction = stagate(mock_data.x, mock_data.edge_index)
-    assert node_embeddings.size() == (num_nodes, 2)
-    assert reconstruction.size() == (num_nodes, mock_dim)
+    outputs = stagate(mock_data)
+    assert outputs["embedding"].size() == (num_nodes, 2)
+    assert outputs["logits"].size() == (num_nodes, mock_dim)
+
+
+def test_stagate_vae(stagate_vae, mock_dim, mock_data):
+    num_nodes, in_dim = mock_data.x.size()
+    outputs = stagate_vae(mock_data)
+    assert outputs["embedding"].size() == (num_nodes, 2)
+    assert outputs["mean"].size() == (num_nodes, 2)
+    assert outputs["logvar"].size() == (num_nodes, 2)
+    assert outputs["logits"].size() == (num_nodes, mock_dim)
