e5ae10e6c873590c4c52e22458f0dcd22cedcbe0
Shahul Alam <alam.shahul@gmail.com>
2026-08-17 14:19:22 -0400

    Update config paths.

--- experiments/zhuang_subset/run_JointHopperSketcher.sh
+++ experiments/zhuang_subset/run_JointHopperSketcher.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=/dev/null
 
 name=jointhopper
-OUTDIR="${STEEP_RECORDS_DIR:-experiments/zhuang_subset/records}/$name"
+OUTDIR="experiments/zhuang_subset/records/$name"
 mkdir -p $OUTDIR
 
 ratios=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
